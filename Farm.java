a54520014208071893d8bf046eb7750a184254b7
aparker15 <aparker@music2.class.lcs.org>
2015-03-09 09:45:38 -0400

    Farm

---
+++ Farm.java
@@ -0,0 +1,27 @@
+public class Farm
+{
+  public static void main(String[] a)
+  {
+    World wref = new World();
+    Squirrel bob = new Squirrel(wref);
+        bob.getDistance(100,100);
+  bob.turnToFace(bob);
+  bob.forward(100);
+  bob.turnToFace(10,10);
+  bob.backward(20); 
+  bob.turnToFace(125,10);
+  bob.forward(100);
+  bob.penUp();
+  bob.getDistance(20,10);
+  bob.getDistance(100,100);
+  bob.turnToFace(bob);
+  bob.forward(100);
+  bob.turnToFace(10,10);
+  bob.backward(20); 
+  bob.turnToFace(125,10);
+  bob.forward(100);
+  bob.penUp();
+  bob.getDistance(20,10);
+  }
+
+  }
